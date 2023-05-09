d78413d44ae2f88f3ba2d9c6b2b1b56eca1237a2
xinyudai <50563344+dxy5930@users.noreply.github.com>
2023-05-09 16:05:51 +0800

    Update index.js
    修改请求样例

--- http/api/index.js
+++ http/api/index.js
@@ -1,9 +1,18 @@
-import service from "./index";
+import service from "../request";
 
+// get request
 export function test(data) {
   return service({
     url: "/classification.json",
     method: "get",
     params: data,
+  });
+}
+// post request
+export function test(data) {
+  return service({
+    url: "/classification.json",
+    method: "post",
+    data,
   });
 }
